d39ec873332a7bf2f328610c580b51c6bdab669e
Jakub Wasilewski <wasyl@wasyl.eu>
2016-05-22 17:24:27 +0200

    Clean slate for presentation.

--- chaplain.tests.js
+++
@@ -1,9 +0,0 @@
-suite('bacon', function() {
-  var testApp = require('./app')();
-
-  config({app: testApp});
-
-  test('zombie-html', {url: '/html'});
-  test('3-paragraphs-json', {url: '/json/3'});
-});
-
